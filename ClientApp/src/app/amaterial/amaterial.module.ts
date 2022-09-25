a40c67b78f64741efd5a9e0b64b6be5f95e56f40
mpapechrc <111442881+mpapechrc@users.noreply.github.com>
2022-09-25 18:10:42 +0200

    a

--- ClientApp/src/app/amaterial/amaterial.module.ts
+++ ClientApp/src/app/amaterial/amaterial.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 const MaterialComponents = [
-  MatButtonModule
+  MatButtonModule,
+  MatInputModule
 ];
 
 @NgModule({
