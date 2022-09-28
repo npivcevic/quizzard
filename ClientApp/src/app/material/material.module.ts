7ac14359428f20704a559efff96096612e096504
mpapechrc <111442881+mpapechrc@users.noreply.github.com>
2022-09-28 17:33:16 +0200

    0

--- ClientApp/src/app/material/material.module.ts
+++ ClientApp/src/app/material/material.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle'
 
 
 const MaterialComponents=[
   MatButtonModule,
   MatInputModule,
-  MatDialogModule
+  MatDialogModule,
+  MatSlideToggleModule
 ]
 
 @NgModule({
