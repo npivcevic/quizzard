e5ad2194839fbffdfc283e37684bf43371c2723d
mpapechrc <111442881+mpapechrc@users.noreply.github.com>
2022-10-01 01:08:12 +0200

    feat: isCorrect is single selection

--- ClientApp/src/app/add-question/add-question.component.ts
+++ ClientApp/src/app/add-question/add-question.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { PostQuestion } from '../model/post-question';
 import { QuestionService } from '../question.service';
-import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-question',
@@ -10,12 +9,12 @@ import { FormControl } from '@angular/forms';
 })
 export class AddQuestionComponent implements OnInit {
 
- 
 
   constructor(private questionservice : QuestionService) { }
 
   ngOnInit(): void {
   }
+
 
   postquestion: PostQuestion={
     text: "",
@@ -27,14 +26,35 @@ export class AddQuestionComponent implements OnInit {
     ]
   }
 
-  questionText = new FormControl(this.postquestion.text)
-
   postQuestion(x: PostQuestion){
 
-     
     this.questionservice.postQuestion(x)
   }
 
+  
+  singleToggle( index:number){
+   
+    this.postquestion.answers.forEach(answer => {
+      answer.isCorrect= false
+      if (answer.text=this.postquestion.answers[index].text) {
+        this.postquestion.answers[index].isCorrect=true
+      }
+    });    
+    console.log(this.postquestion)
+  }
+
+  /*
+  singleToggle1( index:number){
+   
+    for (let i=0;i<this.postquestion.answers.length;i++){
+      this.postquestion.answers[i].isCorrect=false
+      if (this.postquestion.answers[i].text=this.postquestion.answers[index].text) {
+        this.postquestion.answers[i].isCorrect=true
+      }
+    }
+    console.log(this.postquestion)
+  }
+  */
   
 
 }
