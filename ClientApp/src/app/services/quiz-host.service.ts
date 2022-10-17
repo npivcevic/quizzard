b63a791c6c8dec9937c80c80efa6a538b8f064ee
mpapechrc <111442881+mpapechrc@users.noreply.github.com>
2022-10-17 21:19:17 +0200

    feat:send object with iscorrect property to player

--- ClientApp/src/app/services/quiz-host.service.ts
+++ ClientApp/src/app/services/quiz-host.service.ts
@@ -12,15 +12,15 @@ export class QuizHostService {
 
   public groupName: string = "";
   public players: Player[] = [];
-  public questions:Question[]=[]
-  public currentquestion:Question={} as Question;
+  public questions: Question[] = []
+  public currentquestion: Question = {} as Question;
   //indicator for start button 
-  public playersJoining:boolean=true
+  public playersJoining: boolean = true
   //indicator for questions 
   public quizStarted: boolean = false
 
-  public playerAnswer:string="";
-  public playerId:string="";
+  public playerAnswer: string = "";
+  public playerId: string = "";
   public curentQuestionIndex: number = -1
   //spinner variables
   public timeLeft: number = 100;
@@ -30,8 +30,8 @@ export class QuizHostService {
   public showingCorrectAnswer: boolean = false
   public nextQuestionDelay: number = 2000
   //indicator for scoreboard
-  public quizEnded:boolean=false
-  public playerScoreboard:Player={} as Player
+  public quizEnded: boolean = false
+  public playerScoreboard: Player = {} as Player
 
 
   constructor(public signalRService: SignalrService, public questionservice: QuestionService) { }
@@ -44,31 +44,31 @@ export class QuizHostService {
     })
 
     this.questionservice.getQuestions()
-    .subscribe(data => this.questions=data)
+      .subscribe(data => this.questions = data)
 
   }
 
   startQuiz() {
     this.quizStarted = true
-    this.playersJoining=false
+    this.playersJoining = false
     this.nextQuestion();
   }
 
-  recordAnswer(playerId:string, answerId:string, questionId:string){
-    this.players.forEach((player)=>{
-      if(player.connectionId===playerId){
+  recordAnswer(playerId: string, answerId: string, questionId: string) {
+    this.players.forEach((player) => {
+      if (player.connectionId === playerId) {
         player.submitedAnswers.push({
           questionId,
           answerId
         })
       }
     })
-    
+
   }
-  
-  sendQuestiontoPlayer(){
-    this.currentquestion=Object.assign({},this.questions[this.curentQuestionIndex])
-    this.currentquestion.answers=this.questions[this.curentQuestionIndex].answers.map((answer)=>{
+
+  sendQuestiontoPlayer() {
+    this.currentquestion = Object.assign({}, this.questions[this.curentQuestionIndex])
+    this.currentquestion.answers = this.questions[this.curentQuestionIndex].answers.map((answer) => {
       return {
         id: answer.id,
         text: answer.text
@@ -82,9 +82,9 @@ export class QuizHostService {
   }
 
   nextQuestion() {
-    if(this.curentQuestionIndex+1<this.questions.length){
+    if (this.curentQuestionIndex + 1 < this.questions.length) {
       this.curentQuestionIndex++
-      console.log(this.curentQuestionIndex,this.questions.length)
+      console.log(this.curentQuestionIndex, this.questions.length)
       this.sendQuestiontoPlayer()
       this.showingCorrectAnswer = false
       this.timeLeft = 100
@@ -96,80 +96,78 @@ export class QuizHostService {
           this.showCorrectAnswer()
         }
       }, this.totalTimePerQuestion / (100 * 2));
-    }else{
-      const data={
-        action:'QuizEnded',
-        data:true
+    } else {
+      const data = {
+        action: 'QuizEnded',
+        data: true
       }
-      let quizEndMessage=JSON.stringify(data)
+      let quizEndMessage = JSON.stringify(data)
       this.sendToGroup(quizEndMessage)
-      this.players.forEach((player)=>{
+      this.players.forEach((player) => {
         this.sendScoreToPlayer(player.connectionId)
       })
       console.log("quiz ended")
-      this.quizStarted=!this.quizStarted
-      this.quizEnded=!this.quizEnded
-      
+      this.quizStarted = !this.quizStarted
+      this.quizEnded = !this.quizEnded
+
     }
   }
 
-  checkAnswerAndAssignPoints(){
-    let correctAnswer = this.questions[this.curentQuestionIndex].answers.find((correctAnswer)=>{
-      return correctAnswer.isCorrect===true
+  checkAnswerAndAssignPoints() {
+    let correctAnswer = this.questions[this.curentQuestionIndex].answers.find((correctAnswer) => {
+      return correctAnswer.isCorrect === true
     })
-    this.players.forEach((player)=>{
-      let x = player.submitedAnswers.find((submitedAsnwer)=>{
-        return submitedAsnwer.questionId===this.currentquestion.id
+    this.players.forEach((player) => {
+      let x = player.submitedAnswers.find((submitedAsnwer) => {
+        return submitedAsnwer.questionId === this.currentquestion.id
       })
 
-      if(x && x.answerId===correctAnswer?.id){
+      if (x && x.answerId === correctAnswer?.id) {
         player.score++
       }
-      const data= {
-        action:"CorrectAnswer",
+      const data = {
+        action: "CorrectAnswer",
         correctAnswerForPlayer: correctAnswer?.id
       }
       let correctAnswerForPlayer = JSON.stringify(data)
       this.sendToGroup(correctAnswerForPlayer)
     })
-    this.players.sort(function(a,b){return b.score - a.score})
+    this.players.sort(function (a, b) { return b.score - a.score })
   }
 
-  findQuestionTextById(questionId:string){
-    const Q=this.questions.find((question)=>{
-      return question.id===questionId
+  findQuestionTextById(questionId: string) {
+    const Q = this.questions.find((question) => {
+      return question.id === questionId
     })
-
     return Q!.text
   }
 
-  findAnswerTextById(questionId:string,answerId:string){
-    let answerText='';
-
-    this.questions.forEach((question)=>{
-      if(question.id===questionId){
-        const A = question.answers.find((answer)=>{
-          return answer.id===answerId
+  findAnswerTextById(questionId: string, answerId: string) {
+    let answerText = '';
+    this.questions.forEach((question) => {
+      if (question.id === questionId) {
+        const A = question.answers.find((answer) => {
+          return answer.id === answerId
         })
-        return answerText=A!.text
+        return answerText = A!.text
       }
       return
     })
     return answerText
   }
 
-  addStyleToPlayerAnswerOnScoreboard(questionId:string,answerId:string){
-    let style={}
+  addStyleToPlayerAnswerOnScoreboard(questionId: string, answerId: string) {
+    let style = {}
 
-    this.questions.forEach((question)=>{
-      if(question.id===questionId){
-        const A = question.answers.find((answer)=>{
-          return answer.id===answerId
+    this.questions.forEach((question) => {
+      if (question.id === questionId) {
+        const A = question.answers.find((answer) => {
+          return answer.id === answerId
         })
-        if(A?.isCorrect===true){
-          return style={'background':'rgb(153, 211, 153)'}
-        }else{
-          return style={'background':'rgb(236, 157, 157)'}
+        if (A?.isCorrect === true) {
+          return style = { 'background': 'rgb(153, 211, 153)' }
+        } else {
+          return style = { 'background': 'rgb(236, 157, 157)' }
         }
       }
       return
@@ -183,30 +181,42 @@ export class QuizHostService {
     setTimeout(() => this.nextQuestion(), this.nextQuestionDelay)
   }
 
-  sendScoreToPlayer(playerId:string){
-    let answerScore=[]
-    const player = this.players.find((player)=>{
-      return player.connectionId===playerId
+  sendScoreToPlayer(playerId: string) {
+    let answerScore:any = []
+    const player = this.players.find((player) => {
+      return player.connectionId === playerId
     })
     console.log("player data found", player)
-    this.playerScoreboard.name=player!.name
-    this.playerScoreboard.submitedAnswers=[]
-    player?.submitedAnswers.forEach((submitedAnswer)=>{
-      let x:SubmitedAnswer={} as SubmitedAnswer;
-      x.questionId=this.findQuestionTextById(submitedAnswer.questionId)
-      x.answerId=this.findAnswerTextById(submitedAnswer.questionId,submitedAnswer.answerId)
-      this.playerScoreboard.submitedAnswers.push(x)
+    this.playerScoreboard.name = player!.name
+    this.playerScoreboard.submitedAnswers = []
+    player?.submitedAnswers.forEach((submitedAnswer) => {
+      let x = this.findQuestionTextById(submitedAnswer.questionId)
+      let y = this.findAnswerTextById(submitedAnswer.questionId, submitedAnswer.answerId)
+      let z = this.questions.forEach((question) => {
+        if (question.id === submitedAnswer.questionId) {
+          const A = question.answers.find((answer) => {
+            return answer.id === submitedAnswer.answerId
+          })
+          return A!.isCorrect
+        }
+        return
+      })
+      answerScore.push({
+        questionText:x,
+        answerText:y,
+        isCorrect:z
+      })
     })
-    const data={
-      action:'PlayerScore',
-      data: this.playerScoreboard
+    const data = {
+      action: 'PlayerScore',
+      data: answerScore
     }
     this.sendToPlayer(JSON.stringify(data), playerId)
     console.log("player data sent", player)
   }
 
-  public sendToPlayer(data:string, playerConnectionId: string){
-    this.signalRService.sendToPlayer(data,playerConnectionId)
+  public sendToPlayer(data: string, playerConnectionId: string) {
+    this.signalRService.sendToPlayer(data, playerConnectionId)
   }
 
   public sendToGroup(data: string) {
@@ -218,16 +228,16 @@ export class QuizHostService {
       case 'QuestionSent':
         break;
       case 'PlayerAnswered':
-        this.playerAnswer=data.data.answerId
-        this.playerId=data.senderConnectionId
+        this.playerAnswer = data.data.answerId
+        this.playerId = data.senderConnectionId
         this.recordAnswer(this.playerId, this.playerAnswer, this.currentquestion.id)
         break;
       case 'GroupCreated':
         this.groupName = data.data
         break;
       case 'PlayerJoined':
-        data.data.submitedAnswers=[]
-        data.data.score=0
+        data.data.submitedAnswers = []
+        data.data.score = 0
         this.players.push(data.data)
         break;
       case 'PlayerDisconnected':
