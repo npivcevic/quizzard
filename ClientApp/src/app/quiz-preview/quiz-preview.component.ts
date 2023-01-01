39fc039b6180edfdb09d7a2c08bd636e2bad1b37
mpapechrc <111442881+mpapechrc@users.noreply.github.com>
2023-01-01 19:13:31 +0100

    quiz preview table

--- ClientApp/src/app/quiz-preview/quiz-preview.component.ts
+++ ClientApp/src/app/quiz-preview/quiz-preview.component.ts
@@ -1,34 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { QuizzesService } from '../services/quizzes.service';
 import { Quiz } from '../model/quiz';
 import { letterFromIndex } from '../utils/letterFromIndex';
+import { QuestionSet } from '../model/question-set';
 
 @Component({
   selector: 'app-quiz-preview',
   templateUrl: './quiz-preview.component.html',
   styleUrls: ['./quiz-preview.component.css']
 })
-export class QuizPreviewComponent implements OnInit {
+export class QuizPreviewComponent {
 
   @Input() quiz!:Quiz
+
+  displayedColumns: string[] = ['name', "questions"];
   
   constructor(private route: ActivatedRoute,
               private quizservice: QuizzesService){}
 
-  ngOnInit(): void {
-    this.route.paramMap.subscribe((params) => {
-      const id = params.get('id')
-      if (!id) {
-        return
-      }
-      this.quizservice.getQuiz(id)
-        .subscribe({
-          next: (data) => {
-            this.quiz = data
-          }
-        })
-    })
+  getTotalCost() {
+    return this.quiz.questionSets.map(t => t.questions.length).reduce((acc, value) => acc + value, 0);
   }
 
   getLetter(n: number) {
