5590119df59a8416e9f2e45433403b367d81c25d
mpapechrc <111442881+mpapechrc@users.noreply.github.com>
2023-11-25 00:16:41 +0100

    change user id to guid

---
+++ Data/Migrations/20231124231117_user-id-to-guid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace quizzard.Data.Migrations
+{
+    public partial class useridtoguid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -1,8 +1,11 @@
-﻿namespace quizzard.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace quizzard.Models
 { 
     public class User
     {
-        public int Id { get; set; }
+        [Key]
+        public Guid Id { get; set; }
         public string Email { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; } = new byte[32];
         public byte[] PasswordSalt { get; set; } = new byte[32];
