0eca5ea235c9f6126e84c67602630a9384f9e93e
mpapechrc <111442881+mpapechrc@users.noreply.github.com>
2024-03-12 23:44:47 +0100

    change http to https

--- Program.cs
+++ Program.cs
@@ -78,7 +78,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 //app.UseHsts();
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAuthentication();
 app.UseRouting();
