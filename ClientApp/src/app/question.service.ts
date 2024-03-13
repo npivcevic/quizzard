89d219f21fb22bc064b0f7c3c09ee3bd2b6af139
mpapechrc <111442881+mpapechrc@users.noreply.github.com>
2024-03-13 18:38:15 +0100

    fix question service base url

--- ClientApp/src/app/question.service.ts
+++ ClientApp/src/app/question.service.ts
@@ -2,35 +2,36 @@ import { Injectable, Inject } from '@angular/core';
 import { Question, PostQuestion } from './model/question';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionService {
 
-  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string,) { }
+  constructor(private http: HttpClient) { }
 
   postQuestion(question: PostQuestion): Observable<Question> {
-    return this.http.post<Question>(this.baseUrl + "api/Questions", question)
+    return this.http.post<Question>(environment.apiUrl + "api/Questions", question)
   }
 
   postQuestionBulk(question: PostQuestion[]): Observable<null> {
-    return this.http.post<null>(this.baseUrl + "api/Questions/Bulk", question)
+    return this.http.post<null>(environment.apiUrl + "api/Questions/Bulk", question)
   }
 
   getQuestions(): Observable<Question[]> {
-    return this.http.get<Question[]>(this.baseUrl + "api/Questions")
+    return this.http.get<Question[]>(environment.apiUrl + "api/Questions")
   }
 
   getRandomQuestions(size: any): Observable<Question[]> {
-    return this.http.get<Question[]>(this.baseUrl + "api/Questions/Random?size=" + size)
+    return this.http.get<Question[]>(environment.apiUrl + "api/Questions/Random?size=" + size)
   }
 
   deleteQuestion(id: string): Observable<null> {
-    return this.http.delete<null>(this.baseUrl + "api/Questions/" + id)
+    return this.http.delete<null>(environment.apiUrl + "api/Questions/" + id)
   }
 
   putQuestion(question: Question): Observable<null> {
-    return this.http.put<null>(this.baseUrl + "api/Questions/" + question.questionId, question)
+    return this.http.put<null>(environment.apiUrl + "api/Questions/" + question.questionId, question)
   }
 }
